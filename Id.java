940c4d2b68c3e561b3da6bee7b4369de6875db17
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 19:04:10 +0530

    upload

---
+++ Id.java
@@ -0,0 +1,16 @@
+class Id{
+static int[]num={1,2,3,4,5,6,7,8,9,10};
+public static void main(String[] args){
+System.out.println(num[0]);
+System.out.println(num[1]);
+System.out.println(num[3]);
+System.out.println(num[4]);
+System.out.println(num[5]);
+System.out.println(num[6]);
+System.out.println(num[7]);
+System.out.println(num[8]);
+System.out.println(num[9]);
+
+}
+
+}
